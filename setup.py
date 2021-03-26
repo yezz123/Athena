047fbee9799ca8612d079459dfaf16caac1e154b
Yasser Tahiri <yasserth19@gmail.com>
2021-03-27 00:42:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License (MIT)",
         "Topic :: Lab :: Web Application",
         "Operating System :: OS Independent"
