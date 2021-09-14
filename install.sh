f023d00f8c68f0ae96c0af1c5cf3f95325ae521b
Yasser Tahiri <yasserth19@gmail.com>
2021-09-14 15:08:35 +0100

    Fix The Mode user issue

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys 7D8D0BF6
-sudo apt-get update
+apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys 7D8D0BF6
+apt-get update
